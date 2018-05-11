cb20f3125aa949726ef7684a6d0531547fd5c6d9
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-05-11 18:25:49 +0100

    Netlify form v2

--- src/components/QuoteForm.jsx
+++ src/components/QuoteForm.jsx
@@ -64,16 +64,11 @@ const Wrapper = styled.section`
 
 const QuoteForm = () => (
   <Wrapper>
-    <form name="contact" netlify>
-      <p>
-        <label>Name <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Email <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
+    <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+      <input name="name" placeholder="Your Name" type="text" />
+      <input name="email" placeholder="name@name.com" type="email" />
+      <textarea name="message" />
+      <button>Send</button>
     </form>
   </Wrapper>
 );
