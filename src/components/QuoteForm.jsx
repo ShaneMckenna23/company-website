4e70e7dad8f4e80a1e8a8f9101502bb6ec2fe051
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-05-11 18:43:24 +0100

    Netlify form v4

--- src/components/QuoteForm.jsx
+++ src/components/QuoteForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'react-emotion';
+import { navigateTo } from 'gatsby-link';
 
 const Wrapper = styled.section`
   .main-content {
@@ -62,15 +63,76 @@ const Wrapper = styled.section`
   }
 `;
 
-const QuoteForm = () => (
-  <Wrapper>
-    <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-      <input name="name" placeholder="Your Name" type="text" />
-      <input name="email" placeholder="name@name.com" type="email" />
-      <textarea name="message" />
-      <button type="submit">Send</button>
-    </form>
-  </Wrapper>
-);
+function encode(data) {
+  return Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&');
+}
+
+class QuoteForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(() => navigateTo('/thanks/'))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Contact</h1>
+        <form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <p hidden>
+            <label>
+              Don’t fill this out: <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your name:<br />
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:<br />
+              <input type="email" name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:<br />
+              <textarea name="message" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default QuoteForm;
