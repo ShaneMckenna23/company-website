c21d923aa92867b55c0f4cdeefccac5b5296d67a
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-05-25 19:04:30 +0100

    Logos

--- src/icons/Logo.jsx
+++ src/icons/Logo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Logo = () => <div style={{ color: 'white'}}>S22</div>;
+const Logo = () => <img src="/favicons/android-chrome-192x192.png" alt="s22 logo" height="64" width="64" />;
 
 export default Logo;
